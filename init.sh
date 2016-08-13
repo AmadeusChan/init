6f697584565710909a8b92fa547f7bd1f207f6ca
AmadeusChan <greenclouds@foxmail.com>
2016-08-13 18:05:22 +0800

    B

--- init.sh
+++ init.sh
@@ -1,12 +1,13 @@
 echo Initializing
 sudo apt-get update
-sudo apt-get install git cmake build-essential indicator-cpufreq lm-sensors emacs r-base openconnect
+echo y | sudo apt-get install git cmake build-essential indicator-cpufreq lm-sensors emacs r-base 
+echo y | sudo apt-get install openconnect
 git clone https://github.com/AmadeusChan/vimsetting.git
 cp ./vimsetting/.vimrc ~/.vimrc
 
 echo Installing Opencv
-sudo apt-get install libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+echo y | sudo apt-get install libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+echo y | sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 wget https://codeload.github.com/opencv/opencv/zip/2.4.13
 unzip 2.4.13
 cd opencv-2.4.13/
@@ -22,9 +23,9 @@ sudo ldconfig
 echo Installing Caffe 
 cd ../..
 git clone https://github.com/BVLC/caffe.git
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
-sudo apt-get install --no-install-recommends libboost-all-dev
-sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
+echo y | sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+echo y | sudo apt-get install --no-install-recommends libboost-all-dev
+echo y | sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
 cp ./Makefile.config ./caffe/Makefile.config
 cd caffe
 make all -j8
